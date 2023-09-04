a13e3ac3cb94829d010975bb0e8724dabe0975dd
hsuanyc <hsuanyin81@gmail.com>
2023-09-05 01:33:50 +0800

    Updated gitignore and modified the title of the app

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import os
 import eel
 from spiders.book_spider import main as spider_main  # 導入爬蟲的main()，並重新命名避免衝突
 
-eel.init('web')  # 初始化Eel和Web資料夾
+eel.init(f'{os.path.dirname(os.path.realpath(__file__))}/web')  # 初始化Eel和Web資料夾
 
 
 @eel.expose  # 使用這個裝飾器來讓JavaScript可以訪問這個Python函數
@@ -12,4 +13,4 @@ def start_crawl(output_path):
     spider_main(callback=update_progress)
 
 
-eel.start('index.html', size=(300, 200))
+eel.start('index.html', size=(300, 200), port=8001)
