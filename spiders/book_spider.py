fbdbe919af6a112dacae2be6a15ba106f53bd977
hsuanyc <hsuanyin81@gmail.com>
2023-10-08 16:34:41 +0800

    Added input of file path to update

--- spiders/book_spider.py
+++ spiders/book_spider.py
@@ -1,8 +1,9 @@
 import requests
-from bs4 import BeautifulSoup
 import logging
 import io
 import os
+import re
+from bs4 import BeautifulSoup
 from ebooklib import epub
 
 # 設定日誌
@@ -74,6 +75,60 @@ def get_chapter_size(chapter):
     size = buffer.tell()
     buffer.close()
     return size
+
+
+def get_last_chapter_title_epub(path):
+    book = epub.read_epub(path)
+
+    # 獲取 spine 中的最後一個項目的 ID
+    last_item_id = book.spine[-1][0]
+
+    # 使用 ID 從書籍物件中獲取對應的項目
+    last_item = book.get_item_with_id(last_item_id)
+
+    # 返回項目（章節）的標題
+    content = last_item.get_body_content().decode('utf-8')
+    soup = BeautifulSoup(content, 'html.parser')
+    h1_tag = soup.find('h1')  # 找到第一个<h1>标签
+
+    if h1_tag:  # 确保找到了一个<h1>标签
+        title = h1_tag.text  # 获取标签内的文本
+        logger.info(f"Get last chapter title: Title is {title}")
+        print(f"The title is: {title}")
+    else:
+        logger.info(f"Get last chapter title: No <h1> tag found.")
+        print("No <h1> tag found.")
+    return title
+
+
+def get_latest_chapter_from_web(book_url):
+    response = requests.get(book_url, headers=headers)
+    soup = BeautifulSoup(response.text, 'lxml')
+    latest_chapter = soup.select_one('p:-soup-contains("最新") a').text
+    return latest_chapter
+
+
+def calculate_chapters_to_update(book_url, path):
+    last_title = get_last_chapter_title_epub(path)
+    latest_title = get_latest_chapter_from_web(book_url)
+    if last_title != latest_title:
+        # parse to get number and calculate how many chapters need to be added
+        # 使用正则表达式找到数字
+        last_chapter_match = re.search(r'\d+', last_title)
+        latest_chapter_match = re.search(r'\d+', last_title)
+
+        if last_chapter_match and latest_chapter_match:
+            last_chapter_number = int(
+                last_chapter_match.group(0))  # 获取匹配到的数字字符串
+            latest_chapter_number = int(
+                latest_chapter_match.group(0))  # 将字符串转为整数
+            return abs(last_chapter_number, latest_chapter_number)
+        else:
+            logger.info(
+                "Calculate chapters to update: No chapter number found.")
+            print("No chapter number found.")
+    else:
+        return 0
 
 
 def save_epub(path, epub_chapters, book):
@@ -155,12 +210,25 @@ def create_epub(book_info, chapters):
             path + f'/{book_info["book_title"]}({book_counter}).epub', epub_chapters, book)
 
 
+# def update_epub(book_url, path):
+#     number_to_update = calculate_chapters_to_update(path)
+#     if number_to_update > 0:
+#         fetch_chapters_list(book_url)
+
+
 def main(params, callback=None):
+    # Get params
+    action = params.get("action")
+    epub_path = params.get("epub_path")
+    input_path = params.get("input_path")
+
     book_url = 'https://m.bqg9527.com/zh_hant/book/118028/'
-    book_info = fetch_book_info(book_url)
-    chapters_info = fetch_chapters_list(book_url)
-    total_chapters = len(chapters_info)
-    chapters = []
+
+    if action == "get":
+        book_info = fetch_book_info(book_url)
+        chapters_info = fetch_chapters_list(book_url)
+        total_chapters = len(chapters_info)
+        chapters = []
     # # Testing
     # for n in range(10):
     #     title = chapters_info[n][0]
@@ -169,45 +237,62 @@ def main(params, callback=None):
     #     chapters.append((title, content))
 
     # Production
-    for idx, (title, url) in enumerate(chapters_info):
-        content = fetch_chapter_content(url)
-        chapters.append((title, content))
+        for idx, (title, url) in enumerate(chapters_info):
+            content = fetch_chapter_content(url)
+            chapters.append((title, content))
 
-        # 這裡使用回調函數來更新進度
-        if callback:
-            progress = ((idx + 1) / total_chapters) * 100  # 計算進度百分比
-            callback(
-                f"Downloading chapter {idx+1} of {total_chapters}, {progress:.2f}% complete")
+            # 這裡使用回調函數來更新進度
+            if callback:
+                progress = ((idx + 1) / total_chapters) * 100  # 計算進度百分比
+                callback(
+                    f"Downloading chapter {idx+1} of {total_chapters}, {progress:.2f}% complete")
 
-    create_epub(book_info, chapters)
-    # create_epub('Sample eBook', chapters)
-    logger.info("Ebook creation process completed.")
+        create_epub(book_info, chapters)
+        # create_epub('Sample eBook', chapters)
+        logger.info("Ebook creation process completed.")
+
+    elif action == "update":
+        print("update")
+        # epub_count = get_epub_chapter_count(epub_path)
+        # web_count = get_website_chapter_count(book_url)
+
+        # if epub_count == web_count:
+        #     eel.show_status("The ebook is already up-to-date.")
+        # else:
+        #     eel.show_status("Updating ebook...")
+        #     update_epub(epub_path, book_url)
+        #     eel.show_status("Update complete.")
+
+
+def test():
+    print("Start")
+    html = """
+    <html>
+    <head></head>
+    <body>
+    <div class="block">
+        <div class="block_img2"><img alt="上門龍婿" src="/files/article/image/118/118028/118028s.jpg" border="0" width="100" height="130"></div>
+        <div class="block_txt2">
+            <h2><a>上門龍婿</a></h2>
+            <p>作者：<a href="/zh_hant/author/葉辰/">葉辰</a></p>
+            <p>分類：<a href="/zh_hant/sort/3/">都市言情</a></p>
+            <p>狀態：連載中</p>
+            <p>更新：2023-09-15</p>
+            <p>最新：<a href="209265592.html">第2874章 送給公子的禮物</a></p>
+        </div>
+    </div>
+    </body>
+    </html>
+    """
+    soup = BeautifulSoup(html, 'lxml')
+    print("setup soup")
+    print(soup.select_one('p:-soup-contains("最新") a').text)
 
 
 if __name__ == '__main__':
     try:
         main()
-        # For Testing
-        # print("Start")
-        # html = """
-        # <html>
-        # <head></head>
-        # <body>
-        # <ul id="last12" class="chapter">
-        # <li><a href="/zh_hant/book/118028/209265592.html">第2874章 送給公子的禮物<span></span></a></li>
-        # </ul>
-        # <ul class="chapter">
-        # <li><a href="/zh_hant/book/118028/209265592.html">第2874章 送給公子的禮物<span></span></a></li>
-        # <li><a href="#header" style="color:#1676a7;">==&gt;返回頂部</a></li>
-        # </ul>
-        # </body>
-        # </html>
-        # """
-        # soup = BeautifulSoup(html, 'lxml')
-        # print("setup soup")
-        # print(soup.select(
-        #     'ul.chapter[id!="last12"] > li > a[href!="#header"]'
-        # ))
+        # test()
 
     except Exception as e:
         logger.error(f"An error occurred: {e}")
--- spiders/parse_test.py
+++ spiders/parse_test.py
@@ -3,16 +3,28 @@ import sys
 import ebooklib
 from ebooklib import epub
 from ebooklib.utils import debug
+from bs4 import BeautifulSoup
 
 
 # read book
 book = epub.read_epub(sys.argv[1])
 
-debug(book.metadata)
-print("spine")
-debug(book.spine)
-print("Toc")
-debug(book.toc)
+# debug(book.metadata)
+# print("spine")
+# debug(book.spine)
+# print("Toc")
+# debug(book.toc)
+
+last_item_id = book.spine[-1][0]
+
+# 使用 ID 從書籍物件中獲取對應的項目
+last_item = book.get_item_with_id(last_item_id)
+
+# 返回項目（章節）的標題
+content = last_item.get_body_content().decode('utf-8')
+soup = BeautifulSoup(content, 'html.parser')
+h1_tag = soup.find('h1').text
+print(h1_tag)
 
 for x in book.get_items_of_type(ebooklib.ITEM_IMAGE):
     debug(x)
